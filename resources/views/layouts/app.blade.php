bd3a74c7cf07f5edf99253f4469442aa7899627e
Azhar <33951589+qctfw@users.noreply.github.com>
2021-06-18 14:59:10 +0700

    Add Livewire Package

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - {{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <livewire:styles />
     <script src="https://unpkg.com/alpinejs@3.0.6/dist/cdn.min.js" defer></script>
 </head>
 <body class="font-sans dark:bg-gray-800 dark:text-white">
@@ -17,5 +18,6 @@
             <p>Powered by <a href="https://jikan.moe">Jikan.moe</a> API</p>
         </div>
     </footer>
+    <livewire:scripts />
 </body>
 </html>
