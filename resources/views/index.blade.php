09a63b3b4ea86f77cf9534f61c84a3e329f1451a
Azhar <33951589+qctfw@users.noreply.github.com>
2021-06-28 11:54:54 +0700

    Add link and modify header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,13 @@
     <x-slot name="title">Top Anime</x-slot>
 
     <div class="container px-4 pt-12 mx-auto">
-        <h2 class="text-lg font-semibold tracking-wider text-blue-700 uppercase dark:text-blue-300">Top Anime</h2>
+        <a href="{{ route('top.popular') }}" class="text-lg font-semibold tracking-wider text-blue-700 uppercase dark:text-blue-300">Top Anime</a>
         <div class="grid items-end grid-cols-2 gap-8 py-4 md:grid-cols-3 lg:grid-cols-6">
             @foreach ($top_animes as $anime)
             <x-anime-card-cover :anime="$anime" :resources="$top_resources[$anime['mal_id']]" />
             @endforeach
         </div>
-        <h2 class="pt-12 text-lg font-semibold tracking-wider text-blue-700 uppercase dark:text-blue-300">Anime Paling Dinantikan</h2>
+        <a href="{{ route('top.upcoming') }}" class="pt-12 text-lg font-semibold tracking-wider text-blue-700 uppercase dark:text-blue-300">Anime Paling Dinantikan</a>
         <div class="grid items-end grid-cols-2 gap-8 py-4 md:grid-cols-3 lg:grid-cols-6">
             @foreach ($upcoming_animes as $anime)
             <x-anime-card-cover :anime="$anime" />
