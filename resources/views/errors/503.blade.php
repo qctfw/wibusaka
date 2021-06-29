595bef4511662cc7bb563008e0eae43e5e81d764
Azhar <33951589+qctfw@users.noreply.github.com>
2021-06-29 16:39:37 +0700

    Add 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,4 @@
+<x-error-layout>
+    <x-slot name="code">503</x-slot>
+    <x-slot name="message">Saat ini, halaman yang ingin anda kunjungi tidak tersedia. Mohon maaf atas ketidaknyamanannya.</x-slot>
+</x-error-layout>
