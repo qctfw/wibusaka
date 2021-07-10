7f5e607a0ee970a4d52a995fced8f9578a0be97e
Azhar <33951589+qctfw@users.noreply.github.com>
2021-07-10 21:50:30 +0700

    Small change on Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <footer class="mt-4 border-t border-gray-200 footer dark:border-gray-700">
         <div class="container flex flex-col items-center justify-between gap-1 px-4 py-6 mx-auto md:flex-row">
             <div class="flex flex-row items-center gap-4">
-                <p>2021 &bull; WibuList</p>
+                <p>WibuList <span class="select-none">&bull;</span> 2021</p>
                 <div class="flex-row items-center hidden gap-4 text-gray-700 md:flex dark:text-white">
                     <a href="{{ config('anime.link.github') }}" target="_blank">
                         <x-icons.github class="w-6 h-6 transition-colors duration-150 hover:text-gray-500 dark:hover:text-gray-300" fill="currentColor" />
@@ -28,7 +28,7 @@
                     </a>
                 </div>
             </div>
-            <p>Powered by <a href="https://jikan.moe">Jikan.moe</a> API</p>
+            <p>Powered by <a href="https://jikan.moe" class="font-semibold hover:underline">Jikan.moe</a> API</p>
             <div class="flex flex-row items-center gap-4 text-gray-700 md:hidden dark:text-white">
                 <a href="{{ config('anime.link.github') }}" target="_blank">
                     <x-icons.github class="w-6 h-6" fill="currentColor" />
