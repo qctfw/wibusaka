1b16761d0c12b43d41ca0015c13ce5a4a6d00748
Azhar <33951589+qctfw@users.noreply.github.com>
2021-07-10 08:39:17 +0700

    Add GitHub and Twitter logos on Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,27 @@
     <x-header-navbar />
     {{ $slot }}
     <footer class="mt-4 border-t border-gray-200 footer dark:border-gray-700">
-        <div class="container flex flex-col items-center justify-between px-4 py-6 mx-auto md:flex-row">
-            <p>2021 &bull; WibuList</p>
+        <div class="container flex flex-col items-center justify-between gap-1 px-4 py-6 mx-auto md:flex-row">
+            <div class="flex flex-row items-center gap-4">
+                <p>2021 &bull; WibuList</p>
+                <div class="flex-row items-center hidden gap-4 text-gray-700 transition-colors duration-150 md:flex dark:text-white">
+                    <a href="https://github.com/qctfw/wibulist" target="_blank">
+                        <x-icons.github class="w-6 h-6 hover:text-gray-500 dark:hover:text-gray-300" fill="currentColor" />
+                    </a>
+                    <a href="#" target="_blank">
+                        <x-icons.twitter class="w-6 h-6 hover:text-gray-500 dark:hover:text-gray-300" fill="currentColor" />
+                    </a>
+                </div>
+            </div>
             <p>Powered by <a href="https://jikan.moe">Jikan.moe</a> API</p>
+            <div class="flex flex-row items-center gap-4 text-gray-700 md:hidden dark:text-white">
+                <a href="https://github.com/qctfw/wibulist" target="_blank">
+                    <x-icons.github class="w-6 h-6" fill="currentColor" />
+                </a>
+                <a href="#" target="_blank">
+                    <x-icons.twitter class="w-6 h-6" fill="currentColor" />
+                </a>
+            </div>
         </div>
     </footer>
     <livewire:scripts />
