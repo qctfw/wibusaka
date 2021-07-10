9cca79eeb156c976bc59f1ac440656c283b02c86
Azhar <33951589+qctfw@users.noreply.github.com>
2021-07-10 21:32:24 +0700

    Fix resources on Anime Card List

--- resources/views/components/anime-card-list.blade.php
+++ resources/views/components/anime-card-list.blade.php
@@ -1,14 +1,14 @@
 <div class="relative flex flex-col items-center justify-between p-2 transition-colors bg-gray-200 rounded-lg group hover:bg-gray-300 dark:hover:bg-gray-700 md:static md:flex-row dark:bg-gray-900 md:h-auto">
     @if ($anime['rank'] <= 10)
-    <div class="absolute top-0 left-0 w-auto px-2 text-3xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
+    <div class="absolute top-0 left-0 w-auto px-2 text-2xl md:text-3xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
         #{{ $anime['rank'] }}
     </div>
     @elseif ($anime['rank'] <= 100)
-    <div class="absolute top-0 left-0 w-auto px-2 text-2xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
+    <div class="absolute top-0 left-0 w-auto px-2 text-xl md:text-2xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
         #{{ $anime['rank'] }}
     </div>
     @else
-    <div class="absolute top-0 left-0 w-auto px-2 text-xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
+    <div class="absolute top-0 left-0 w-auto px-2 text-lg md:text-xl text-center bg-gray-200 rounded-lg md:bg-transparent md:static md:block md:flex-none md:w-12 md:px-0 dark:bg-gray-900 md:rounded-none md:font-bold">
         #{{ $anime['rank'] }}
     </div>
     @endif
@@ -51,6 +51,7 @@
             <x-icons.star-solid class="w-6 h-6 pr-1" />
             <span class="text-xl font-semibold">{{ $anime['score'] }}</span>
         </div>
+        @if (!blank($resources))
         <div class="flex-row items-center justify-center mx-4 md:hidden">
             <div class="flex flex-row items-center justify-center gap-3 pt-2 text-sm text-center">
                 @foreach ($resources as $resource)
@@ -58,5 +59,6 @@
                 @endforeach
             </div>
         </div>
+        @endif
     </div>
 </div>
