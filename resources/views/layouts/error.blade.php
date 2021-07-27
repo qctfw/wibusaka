eb96d60acd31e644924273f77d037a85c86f3960
Azhar <33951589+qctfw@users.noreply.github.com>
2021-07-27 20:36:17 +0700

    Add robots meta tag on error page

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <title>{{ $code }} - {{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
