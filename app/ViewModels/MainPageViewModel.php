82a7c5097aa0298783f0fc861b309379bfd0e2fd
Azhar <33951589+qctfw@users.noreply.github.com>
2021-10-10 09:48:38 +0700

    Fix Tailwind Class not found

--- app/ViewModels/MainPageViewModel.php
+++ app/ViewModels/MainPageViewModel.php
@@ -19,8 +19,8 @@ class MainPageViewModel extends ViewModel
     {
         $quotes = (rand(1, 100) <= 5 || env('TITLE_ABNORMAL_HACK', false)) ? $this->quotes['abnormal'] : $this->quotes['normal'];
 
-        $selected_quote = preg_replace('/(?<!\*)\*\*(?![\s*])(.*?)(?<![\s*])\*\*(?!\*)/', '<span class="text-green-500">$1</span>',  $quotes[array_rand($quotes)]);
-        
+        $selected_quote = $quotes[array_rand($quotes)];
+
         return $selected_quote;
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <section class="flex items-center justify-center h-screen text-center">
         <div class="flex flex-col items-center w-full lg:w-2/3 gap-4">
             <h1 class="text-4xl lg:text-7xl font-bold font-primary">
-                {!! $selected_quote !!}
+                {!! preg_replace('/(?<!\*)\*\*(?![\s*])(.*?)(?<![\s*])\*\*(?!\*)/', '<span class="text-green-500">$1</span>', $selected_quote) !!}
             </h1>
             <p class="text-lg lg:text-xl">{{ __('anime.main.subtitle') }}</p>
             <div class="w-1/2 lg:w-1/4 mt-4">
