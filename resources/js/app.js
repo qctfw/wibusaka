7c22fd3b4c0e9c4668360ef9e42a67cf3f413899
Azhar <33951589+qctfw@users.noreply.github.com>
2023-01-19 14:40:28 +0700

    Remove Loading Screen files completely

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,19 +3,6 @@ import imgpreload from './imgpreload'
 import themeselector from './themeselector'
 
 document.addEventListener('DOMContentLoaded', imgpreload)
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    var opacity = 1
-    var loadingScreen = window.setInterval( function () {
-        document.getElementById('loading-screen').style.opacity = opacity
-        opacity = opacity - 0.25
-        if (opacity <= 0) {
-            document.getElementById('loading-screen').remove()
-            window.clearInterval(loadingScreen)
-        }
-    }, 100)
-})
-*/
 document.addEventListener('DOMContentLoaded', themeselector)
 
 Livewire.on('rebuildImgPreload', imgpreload)
